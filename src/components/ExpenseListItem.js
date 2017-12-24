9f6784495c85e74ea76f4aaf9123b042a7c88b1c
Mario Vergara <mario.a.vergara.l@gmail.com>
2017-12-23 23:00:34 -0300

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
         <h3><NavLink to={`/edit/${id}`} activeClassName="is-active" exact={true}>{description}</NavLink></h3>
-        <p>Amount: {`USD$${(amount / 100)} `}-{` Date: ${moment(createdAt).format('DD/MM/YYYY HH:mm:ss').toString()}`}</p>
+        <p>Amount: {`USD${numeral(amount / 100).format('$0,0.00')} `}
+        -
+        {` Date: ${moment(createdAt).format('DD/MM/YYYY HH:mm:ss').toString()}`}</p>
     </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
