33e90a830ae7027fc0ff3bf108c07dbb2c42f7dc
Mario Vergara <mario.a.vergara.l@gmail.com>
2017-12-26 12:11:16 -0300

    Fixing some problems with filters

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
-import totalExpensesSelector from '../selectors/expenses-total'
+import selectExpensesTotal from '../selectors/expenses-total'
+import selectExpenses from '../selectors/expenses'
 
-export class ExpensesSummary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.expensesLength = this.props.expenses.length
-    this.expensesTotal = `USD${numeral(totalExpensesSelector(this.props.expenses) / 100).format('$0,0.00')} `
-  }
-  render() {
-    return (
-      <div>
-        <h1>Expenses Summary</h1>
-        {this.props.expenses.length>0 ? <p>Viewing {this.expensesLength} totalling {this.expensesTotal}</p> : <p>No summary</p>}
-      </div>
-    )
-  }
+export const ExpensesSummary = ({ expensesLength, expensesTotal }) => {
+
+  const expensesTotalFormatted = `USD${numeral(expensesTotal / 100).format('$0,0.00')} `
+  const expenseWord = expensesLength === 1 ? 'expense' : 'expenses'
+  return (
+    <div>
+      <h1>Expenses Summary</h1>
+      {expensesLength > 0 ? <p>Viewing {expensesLength} {expenseWord} totalling {expensesTotalFormatted}</p> : <p>No summary</p>}
+    </div>
+  )
 }
 
-const mapStateToProps = (state, props) => ({
-  expenses: state.expenses
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
+  return {
+    expensesLength: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  }
 }
-)
 
 export default connect(mapStateToProps)(ExpensesSummary)
--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -4,11 +4,16 @@ import { shallow } from 'enzyme'
 import { ExpensesSummary } from '../../components/ExpensesSummary'
 
 test('should render Summary with no expenses correctly', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={[]}/>)
+  const wrapper = shallow(<ExpensesSummary expensesLength={0} expensesTotal={0}/>)
   expect(wrapper).toMatchSnapshot();
 })
 
-test('should render Summary with expenses correctly', () => {
-  const wrapper = shallow(<ExpensesSummary expenses={expenses}/>)
+test('should render Summary with 1 expense correctly', () => {
+  const wrapper = shallow(<ExpensesSummary expensesLength={1} expensesTotal={123}/>)
+  expect(wrapper).toMatchSnapshot();
+})
+
+test('should render Summary with multiple expenses correctly', () => {
+  const wrapper = shallow(<ExpensesSummary expensesLength={10} expensesTotal={123123}/>)
   expect(wrapper).toMatchSnapshot();
 })
